bf962128a558785aad29fd1705c2cab832f29cb4
devDaviSousa <devsousadavi@gmail.com>
2021-11-29 12:40:55 -0300

    Finalizando  o model e formulario

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,29 @@
 import { Dashboard } from "./components/Dashboard";
+import Modal from 'react-modal'
 import { Header } from "./components/Header";
+import { NewTransactionModal } from "./components/NewTransactionsModal";
 import { GlobalStyle } from "./styles/global";
+import { useState } from "react";
+
+Modal.setAppElement('#root')
 
 export function App() {
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
+
+  function handeOpenNewTransactionModalOpen() {
+    setIsNewTransactionModalOpen(true);
+  }
+
+  function handeCloseNewTransactionModalOpen() {
+    setIsNewTransactionModalOpen(false);
+  }
+
   return (
     <div className="App">
       <>
-        <Header />
+        <Header onOpenNewTransactionModal={handeOpenNewTransactionModalOpen} />
         <Dashboard />
+        <NewTransactionModal isOpen={isNewTransactionModalOpen} onRequestClose={handeCloseNewTransactionModalOpen} />
         <GlobalStyle />
       </>
     </div>
--- src/index.tsx
+++ src/index.tsx
@@ -1,25 +1,47 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createServer } from 'miragejs'
+import { createServer, Model } from 'miragejs'
 import { App } from './App';
 
+
 createServer({
+  models: {
+    transactions: Model,
+  },
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
+        {
+          id: 0,
+          title: "Freelance",
+          type: 'deposit',
+          category: 'dev',
+          amount: 1000,
+          createdAt: new Date('2021-02-12 09:00:00')
+        },
+        {
+          id: 1,
+          title: "Aluguel",
+          type: 'withdraw',
+          category: 'dev',
+          amount: 1000,
+          createdAt: new Date('2021-02-12 09:00:00')
+        }
+      ]
+    })
+  },
+
   routes() {
     this.namespace = 'api';
 
-    this.get('/transaction', () => {
-      return [
-        {
-          id: 1,
-          title: 'transaction 1',
-          amount: 400,
-          type: 'deposit',
-          category: 'food',
-          createdAt: new Date()
-        }
-      ];
+    this.get('/transactions', () => {
+      return this.schema.all('transactions')
+    });
 
-    })
+    this.post('/transactions', (schema, request) => {
+      const data = JSON.parse(request.requestBody)
+      return schema.create('transactions', data)
+    });
   }
 })
 
