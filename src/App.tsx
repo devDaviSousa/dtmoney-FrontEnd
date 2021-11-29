f6207e3ec67ae9c002213d1a3d9d7b81cf2c7b90
devDaviSousa <devsousadavi@gmail.com>
2021-11-29 17:19:22 -0300

    Criando contexto de Trasactions

--- src/App.tsx
+++ src/App.tsx
@@ -4,29 +4,34 @@ import { Header } from "./components/Header";
 import { NewTransactionModal } from "./components/NewTransactionsModal";
 import { GlobalStyle } from "./styles/global";
 import { useState } from "react";
+import { TransactionsProvider } from "./TransactionsContext";
 
 Modal.setAppElement('#root')
 
 export function App() {
   const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
 
-  function handeOpenNewTransactionModalOpen() {
+  function handeOpenNewTransactionModal() {
     setIsNewTransactionModalOpen(true);
   }
 
-  function handeCloseNewTransactionModalOpen() {
+  function handeCloseNewTransactionModal() {
     setIsNewTransactionModalOpen(false);
   }
 
   return (
-    <div className="App">
-      <>
-        <Header onOpenNewTransactionModal={handeOpenNewTransactionModalOpen} />
-        <Dashboard />
-        <NewTransactionModal isOpen={isNewTransactionModalOpen} onRequestClose={handeCloseNewTransactionModalOpen} />
-        <GlobalStyle />
-      </>
-    </div>
+
+
+    <TransactionsProvider>
+      <Header onOpenNewTransactionModal={handeOpenNewTransactionModal} />
+      <Dashboard />
+      <NewTransactionModal isOpen={isNewTransactionModalOpen} onRequestClose={handeCloseNewTransactionModal} />
+      <GlobalStyle />
+
+    </TransactionsProvider>
+
+
+
   );
 }
 
